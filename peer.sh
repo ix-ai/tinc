1119c1abdaa0d08f5e00d28d45c6560cd9cdb323
Alex Thomae <alex@thom.ae>
2020-01-26 00:06:51 +0100

    Fixes the variable name

--- peer.sh
+++ peer.sh
@@ -27,12 +27,12 @@ mkdir -p "/etc/tinc/${NETNAME}/peers/${PEER_NAME}/tinc/${NETNAME}/hosts"
 
 cd "/etc/tinc/${NETNAME}/peers/${PEER_NAME}/tinc/${NETNAME}"
 
-cp "/etc/tinc/${NETNAME}/hosts/${SERVER}" "hosts/${SERVER}"
+cp "/etc/tinc/${NETNAME}/hosts/${SERVER_NAME}" "hosts/${SERVER_NAME}"
 
 cat > tinc.conf <<_EOF_
 Name = ${PEER_NAME}
 Interface = tun0
-ConnectTo = ${SERVER}
+ConnectTo = ${SERVER_NAME}
 _EOF_
 
 cat > "hosts/${PEER_NAME}" <<_EOF_
@@ -63,7 +63,7 @@ sudo /sbin/ip addr del ${PEER_ADDR} dev \$INTERFACE
 sudo /sbin/ip link set \$INTERFACE down
 _EOF_
 
-cat > "hosts/${SERVER}-up" <<"_EOF_"
+cat > "hosts/${SERVER_NAME}-up" <<"_EOF_"
 #!/bin/sh
 ORIGINAL_GATEWAY=$(/sbin/ip route show | grep ^default | cut -d ' ' -f 2-3)
 sudo /sbin/ip route add $REMOTEADDRESS $ORIGINAL_GATEWAY
@@ -71,7 +71,7 @@ sudo /sbin/ip route add 0.0.0.0/1 dev $INTERFACE
 sudo /sbin/ip route add 128.0.0.0/1 dev $INTERFACE
 _EOF_
 
-cat > "hosts/${SERVER}-down" <<"_EOF_"
+cat > "hosts/${SERVER_NAME}-down" <<"_EOF_"
 #!/bin/sh
 ORIGINAL_GATEWAY=$(/sbin/ip route show | grep ^default | cut -d ' ' -f 2-3)
 sudo /sbin/ip route del $REMOTEADDRESS $ORIGINAL_GATEWAY
@@ -79,7 +79,7 @@ sudo /sbin/ip route del 0.0.0.0/1 dev $INTERFACE
 sudo /sbin/ip route del 128.0.0.0/1 dev $INTERFACE
 _EOF_
 
-chmod +x tinc-up tinc-down "hosts/${SERVER}-up" "hosts/${SERVER}-down"
+chmod +x tinc-up tinc-down "hosts/${SERVER_NAME}-up" "hosts/${SERVER_NAME}-down"
 
 cd "/etc/tinc/${NETNAME}/peers"
 tar czf "${PEER_NAME}.tar.gz" "${PEER_NAME}"
